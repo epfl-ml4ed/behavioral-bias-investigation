44f36101bc6c151fba8bee123a009b6be04f2e1c
Jadouille <jade.cock@hotmail.com>
2024-02-17 21:33:26 +0100

    DSM tuglet sequencer example

---
+++ src/pattern_mining/features/tuglet_lasat.py
@@ -0,0 +1,37 @@
+import numpy as np
+from features.sequencers.sequencer_tuglet import TugletSequencer
+
+class LasatTugletSequencer(TugletSequencer):
+    """
+    This class prepares the DSM dataset for the tuglet dataset
+
+    
+    """
+
+    def __init__(self, settings):
+        super().__init__(dict(settings))
+        self._name = 'lasta_chemlab'
+        self._notation = 'las_chem'
+        self._dataset = 'lasta_chem'
+
+    def _vector_to_feature(self, timestep):
+        raise NotImplementedError
+
+    def _create_features(self, student):
+        string_student = [self._vector_to_feature(timestep) for timestep in student]
+        features = '; '.join(string_student)
+        return features
+
+    def _create_sequence(self, **kwargs):
+        return self._create_features(kwargs['sequence'])
+
+                
+
+
+            
+
+
+
+
+
+
